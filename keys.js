11ac1dfc462082c0d9f9ad7a038680be3bff09b8
Skylar Siagel <skylarsiagel@Skylars-MacBook-Pro.local>
2017-04-02 23:51:10 -0400

    added twitter keys

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'kYRwKcNEvXWrT52H13Dzh61oLJYmnUvKVoyAzJc8IuQOeOf0Yc',
+  consumer_secret: 'kYRwKcNEvXWrT52H13Dzh61oLJYmnUvKVoyAzJc8IuQOeOf0Yc',
+  access_token_key: '3958984582-IPGL0Y6arofhdy59AIDU9wfeGzE8yqci9aBEkaA',
+  access_token_secret: 'E2C9Hiet0F9j7ZmyFEPTTxTKnq2XkniVGoFulS263d51O',
+}
