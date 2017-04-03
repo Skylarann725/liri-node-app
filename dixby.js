b52c8e02b54f0855712921e523d3ba7570a5dff7
Skylar Siagel <skylarsiagel@Skylars-MacBook-Pro.local>
2017-04-03 00:29:03 -0400

    installed twitter package

---
+++ dixby.js
@@ -0,0 +1,8 @@
+var twitter = require('twitter');
+var keys = require('./keys.js');
+
+console.log(keys);
+
+keys.get(path, params, callback);
+keys.post(path, params, callback);
+keys.stream(path, params, callback);
