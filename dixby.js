bebda7ebbd39769db85e488d522ff9f4dc355cde
Skylar Siagel <skylarsiagel@Skylars-MacBook-Pro.local>
2017-04-06 22:57:25 -0400

    added code

--- dixby.js
+++ dixby.js
@@ -1,8 +1,49 @@
 var twitter = require('twitter');
-var keys = require('./keys.js');
+var keys = new twitter(require('./keys.js'));
+var operation = process.argv[2]; // global variables
+var names = process.argv[3];
 
 console.log(keys);
 
-keys.get(path, params, callback);
-keys.post(path, params, callback);
-keys.stream(path, params, callback);
+
+var tweets = function() {
+    console.log(keys.twitterKeys);
+    var keys = new twitter(keys.twitterKeys);
+    
+    var params = { screen_name: 'skylarann_725' };
+
+    keys.get('statuses/user_timeline', params, function(error, tweets) {
+
+        if (error) {
+            throw error;
+        }
+
+        console.log(JSON.stringify(tweets, null, 2));
+
+    });
+}
+
+var switchOperation = function(operation, value) {
+    switch (operation) { // switch statement to decide which operation
+        case 'my-tweets':
+        console.log();
+        tweets();
+        break;
+        case 'spotify-this-song':
+        console.log();
+        spotify(value);
+        break;
+        case 'movie-this':
+        console.log();
+        movie(value);
+        break;
+        case 'do-what-it-says':
+        console.log();
+        doThis();
+        break;
+    }
+};
+
+switchOperation(operation, names);
+
+
