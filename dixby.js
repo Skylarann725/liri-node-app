3e0181c0259e49c7a3ee3bf52e779982e5831319
Skylar Siagel <skylarsiagel@Skylars-MacBook-Pro.local>
2017-04-09 22:29:56 -0400

    finished

--- dixby.js
+++ dixby.js
@@ -1,12 +1,12 @@
 var twitter = require('twitter'); // access twitter package
 var keys = (require('./keys.js')); // keys for twitter api
-
+var request = require('request');
 var fs = require('fs');
 
 var operation = process.argv[2]; // global variables
 var names = process.argv[3]; // global variables
 
-function tweets() {
+var tweets = function() {
 
     var client = new twitter(keys.twitterKeys);
 
@@ -18,46 +18,84 @@ function tweets() {
             console.log(error);
         } else {
             for (var i = 0; i < tweets.length; i++) {
-                console.log(JSON.stringify(tweets[i].text));
+                console.log(JSON.stringify(tweets[i].created_at + " " + tweets[i].text));
                 console.log("----------------------------")
             }
         }
     });
 }
 
-//function for getting the artists name from array
-var getArtistNames = function(artist) {
-    return artist.name;
-};
-
 var spotify = function(names) {
 
     var spotify = require('spotify'); // access spotify package - was not working globally
 
-    console.log("song name = " + names);
+    if (names === undefined) {
+        names = 'The Sign';
 
-    if (names === '') {
-        console.log("You need a song Name!!!");
-    } else {
-        spotify.search({ type: 'track', query: names }, function(err, data) {
-            if (err) {
-                console.log('Error occurred: ' + err);
-                return;
-            }
-
-            songinfo = data.tracks.items;
-
-            for (var i = 0; i < 3; i++) {
-                for (var j = 0; j < songinfo[i].artists.length; j++) {
-                    console.log(songinfo[i].artists[j].name);
-                }
-                console.log(songinfo[i].name);
-                console.log(songinfo[i].preview_url);
-                console.log(songinfo[i].album.name);
-                console.log("---------------------")
-            }
-        });
+        console.log("Song Name: " + names);
+        console.log('-----------------------')
     }
+
+    spotify.search({ type: 'track', query: names }, function(err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
+
+        songinfo = data.tracks.items;
+
+        for (var i = 0; i < 3; i++) {
+            console.log('Song Name: ' + songinfo[i].name);
+            for (var j = 0; j < songinfo[i].artists.length; j++) {
+                console.log('Artist: ' + songinfo[i].artists[j].name);
+            }
+            console.log('Preview: ' + songinfo[i].preview_url);
+            console.log('Album: ' + songinfo[i].album.name);
+            console.log('-----------------------')
+        }
+    });
+}
+
+var movie = function(names) {
+
+    if (names === undefined) {
+        names = "Mr. Nobody";
+    }
+
+    request('http://www.omdbapi.com/?t=' + names + '&y=&plot=short&tomatoes=true&r=json', function(error, response, body) {
+        if (names === '') {
+            console.log('Please enter a movie name');
+        }
+
+        if (error) {
+            console.log('Error occurred: ' + error);
+            return;
+        } else {
+            console.log('Title: ' + JSON.parse(body).Title);
+            console.log('Year: ' + JSON.parse(body).Year);
+            console.log('IMBD Rating: ' + JSON.parse(body).imdbRating);
+            console.log('Country Produced: ' + JSON.parse(body).Country);
+            console.log('Language: ' + JSON.parse(body).Language);
+            console.log('Plot: ' + JSON.parse(body).Plot);
+            console.log('Actors: ' + JSON.parse(body).Actors);
+            console.log('Rotten Tomatoes Rating: ' + JSON.parse(body).Ratings[1].Value);
+            console.log('Rotten Tomatoes URL: ' + JSON.parse(body).tomatoURL);
+        }
+    });
+}
+
+var doThis = function() {
+
+    fs.readFile("random.txt", "utf8", function(err, data) {
+
+        if (err) {
+            throw err;
+        }
+
+        var dataArr = data.split(",");
+
+        spotify(dataArr[1]);
+    });
 }
 
 var switchOperation = function(operation, names) {
