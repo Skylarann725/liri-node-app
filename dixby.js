06f9f7e6b085198c932484980e0bfd24a9c696d8
Skylar Siagel <skylarsiagel@Skylars-MacBook-Pro.local>
2017-04-09 20:38:44 -0400

    updated twitter and spotify

--- dixby.js
+++ dixby.js
@@ -1,49 +1,84 @@
-var twitter = require('twitter');
-var keys = new twitter(require('./keys.js'));
+var twitter = require('twitter'); // access twitter package
+var keys = (require('./keys.js')); // keys for twitter api
+
+var fs = require('fs');
+
 var operation = process.argv[2]; // global variables
-var names = process.argv[3];
+var names = process.argv[3]; // global variables
 
-console.log(keys);
+function tweets() {
 
+    var client = new twitter(keys.twitterKeys);
 
-var tweets = function() {
-    console.log(keys.twitterKeys);
-    var keys = new twitter(keys.twitterKeys);
-    
-    var params = { screen_name: 'skylarann_725' };
+    var params = { screen_name: 'skylarann_725' }; // count: 5
 
-    keys.get('statuses/user_timeline', params, function(error, tweets) {
+    client.get('statuses/user_timeline', params, function(error, tweets) {
 
         if (error) {
-            throw error;
+            console.log(error);
+        } else {
+            for (var i = 0; i < tweets.length; i++) {
+                console.log(JSON.stringify(tweets[i].text));
+            }
         }
-
-        console.log(JSON.stringify(tweets, null, 2));
-
     });
 }
 
-var switchOperation = function(operation, value) {
+//function for getting the artists name from array
+    var getArtistNames = function(artist) {
+        return artist.name;
+    };
+
+var spotify = function(names) {
+
+    var spotify = require('spotify'); // access spotify package - was not working globally
+
+    console.log("song name = " + names);
+
+    if (names === '') {
+        console.log("You need a song Name!!!");
+    } else {
+        spotify.search({ type: 'track', query: names }, function(err, data) {
+            if (err) {
+                console.log('Error occurred: ' + err);
+                return;
+            }
+
+            songinfo = data.tracks.items;
+
+            for (var i = 0; i < 3; i++) {
+                console.log(songinfo[i].album.artists[i]);
+                console.log("---------------------")
+                console.log(songinfo[i].name);
+                console.log("---------------------")
+                console.log(songinfo[i].preview_url);
+                console.log("---------------------")
+                console.log(songinfo[i].album.name);
+                console.log("---------------------")
+            }
+        });
+    }
+}
+
+var switchOperation = function(operation, names) {
     switch (operation) { // switch statement to decide which operation
         case 'my-tweets':
-        console.log();
-        tweets();
-        break;
+            console.log();
+            tweets();
+            break;
         case 'spotify-this-song':
-        console.log();
-        spotify(value);
-        break;
+            console.log();
+            spotify(names);
+            break;
         case 'movie-this':
-        console.log();
-        movie(value);
-        break;
+            console.log();
+            movie(names);
+            break;
         case 'do-what-it-says':
-        console.log();
-        doThis();
-        break;
+            console.log();
+            doThis();
+            break;
     }
 };
 
 switchOperation(operation, names);
-
-
--- keys.js
+++ keys.js
@@ -1,7 +1,6 @@
-console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: 'kYRwKcNEvXWrT52H13Dzh61oLJYmnUvKVoyAzJc8IuQOeOf0Yc',
+  consumer_key: 'lQwwwxz0TCBqqIWkDyBcZSjoW',
   consumer_secret: 'kYRwKcNEvXWrT52H13Dzh61oLJYmnUvKVoyAzJc8IuQOeOf0Yc',
   access_token_key: '3958984582-IPGL0Y6arofhdy59AIDU9wfeGzE8yqci9aBEkaA',
   access_token_secret: 'E2C9Hiet0F9j7ZmyFEPTTxTKnq2XkniVGoFulS263d51O',
